af8ae8016ff45eaae0cf5abcc8e3e86b97c3d1a0
Roman Overko <roman.overko@iota.org>
2023-12-19 14:01:12 +0100

    Add Bullshark Quest 1 boundaries

--- python/density.py
+++ python/density.py
@@ -7,12 +7,15 @@ FILE = './../results/workspace1/epoch_density_data.json'
 with open(FILE, 'r') as f:
     json_ = json.load(f);
 
+BULLSHARK_QUEST_1_START = 85
+BULLSHARK_QUEST_1_END = 106
+
 main_df = pd.DataFrame.from_dict(json_['epochs'], orient='index')
 main_df.index = main_df.index.astype(int);
 interval_df = pd.json_normalize(main_df['avg_interval_data'])
 
 plt.rcParams.update({
-    'font.size': 18,
+    'font.size': 16,
     'text.usetex': True,
     'font.family': 'serif',
     'font.serif': ['Times']
@@ -20,14 +23,16 @@ plt.rcParams.update({
 
 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(9, 6), dpi=300)
 
+ax.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
 ax.axhline(y=0, linestyle=':', linewidth=1, color='black')
-#ax.axvline(x=85, linestyle='-.', linewidth=1, color='red', label='Bullshark Quest 1 start')
-ax.plot(main_df['density'], linewidth=2, linestyle='--', marker='o', color='green')
+
+ax.plot(main_df['density'], linewidth=2, linestyle='--', marker='o', color='blue')
+
 #ax.set_title('Percentage of TXs involving shared objects on the Sui mainnet per epoch')
 ax.set_ylabel('Density')
 ax.set_xlabel('Epoch')
 ax.minorticks_on()
-#ax.legend()
+ax.legend()
 
 fig.tight_layout()
 plt.savefig('./../results/workspace1/density.pdf', format='pdf')
--- python/main.py
+++ python/main.py
@@ -8,6 +8,9 @@ FILE = './../results/workspace1/epoch_density_data.json'
 with open(FILE, 'r') as f:
     json_ = json.load(f);
 
+BULLSHARK_QUEST_1_START = 85
+BULLSHARK_QUEST_1_END = 106
+
 main_df = pd.DataFrame.from_dict(json_['epochs'], orient='index')
 main_df.index = main_df.index.astype(int);
 interval_df = pd.json_normalize(main_df['avg_interval_data'])
@@ -18,49 +21,49 @@ fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7) = plt.subplots(nrows=NUM_SUBPLOTS, ncol
 
 print('Total number of scanned TXs: {}'.format(main_df['num_txs_total'].sum()))
 
+ax1.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
 ax1.axhline(y=0, linestyle=':', linewidth=1, color='black')
-ax1.axvline(x=85, linestyle='-.', linewidth=1, color='red', label='Bullshark Quest 1 start')
 ax1.plot(main_df['num_txs_total'], linewidth=2, linestyle='--', marker='o', color='orange')
 ax1.set_title('Total number of TXs on the Sui mainnet per epoch')
 ax1.set_ylabel('TX number')
 ax1.minorticks_on()
 ax1.legend()
 
+ax2.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
 ax2.axhline(y=0, linestyle=':', linewidth=1, color='black')
-ax2.axvline(x=85, linestyle='-.', linewidth=1, color='red', label='Bullshark Quest 1 start')
 ax2.plot(main_df['num_txs_touching_shared_objs'], linewidth=2, linestyle='--', marker='o', color='magenta')
 ax2.set_title('Number of TXs touching shared objects on the Sui mainnet per epoch')
 ax2.set_ylabel('TX number')
 ax2.minorticks_on()
 ax2.legend()
 
+ax3.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
 ax3.axhline(y=0, linestyle=':', linewidth=1, color='black')
-ax3.axvline(x=85, linestyle='-.', linewidth=1, color='red', label='Bullshark Quest 1 start')
 ax3.plot(main_df['density'] * 100, linewidth=2, linestyle='--', marker='o', color='green')
 ax3.set_title('Percentage of TXs involving shared objects on the Sui mainnet per epoch')
 ax3.set_ylabel('Density, %')
 ax3.minorticks_on()
 ax3.legend()
 
+ax4.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
 ax4.axhline(y=0, linestyle=':', linewidth=1, color='black')
-ax4.axvline(x=85, linestyle='-.', linewidth=1, color='red', label='Bullshark Quest 1 start')
 ax4.plot(main_df['num_shared_objects_per_epoch'], linewidth=2, linestyle='--', marker='o', color='olive')
 ax4.set_title('Number of (unique) shared objects on the Sui mainnet per epoch')
 ax4.set_ylabel('Shared object number')
 ax4.minorticks_on()
 ax4.legend()
 
+ax5.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
 ax5.axhline(y=0, linestyle=':', linewidth=1, color='black')
-ax5.axvline(x=85, linestyle='-.', linewidth=1, color='red', label='Bullshark Quest 1 start')
 ax5.plot(main_df['num_shared_objects_total'], linewidth=2, linestyle='--', marker='o', color='blue')
 ax5.set_title('Total number of (unique) shared objects on the Sui mainnet')
 ax5.set_ylabel('Shared object number')
 ax5.minorticks_on()
 ax5.legend()
 
+ax6.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
 ax6.axhline(y=0, linestyle=':', linewidth=1, color='black')
 ax6.axhline(y=1, linestyle='-.', linewidth=1, color='black')
-ax6.axvline(x=85, linestyle='-.', linewidth=1, color='red', label='Bullshark Quest 1 start')
 for col in interval_df:
     if 'degree' in col:
         ax6.plot(interval_df[col], linewidth=2, label='Interval: {} checkpoints'.format(col.split('.')[0]))
@@ -69,8 +72,8 @@ ax6.set_ylabel('Avg contention degree')
 ax6.minorticks_on()
 ax6.legend()
 
+ax7.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
 ax7.axhline(y=0, linestyle=':', linewidth=1, color='black')
-ax7.axvline(x=85, linestyle='-.', linewidth=1, color='red', label='Bullshark Quest 1 start')
 for col in interval_df:
     if not 'degree' in col:
         ax7.plot(interval_df[col], linewidth=2, label='Interval: {} checkpoints'.format(col.split('.')[0]))
