83022c3996945dfde9ad286931db913fd36d1932
Roman Overko <roman.overko@iota.org>
2024-01-26 13:39:59 +0100

    Add more quests to the main figure

--- python/main.py
+++ python/main.py
@@ -10,6 +10,13 @@ with open(FILE, 'r') as f:
 
 BULLSHARK_QUEST_1_START = 85
 BULLSHARK_QUEST_1_END = 106
+BULLSHARK_QUEST_2_START = 107
+BULLSHARK_QUEST_2_END = 146
+BULLSHARK_QUEST_3_START = 183
+BULLSHARK_QUEST_3_END = 211
+WINTER_QUEST_START = 250
+WINTER_QUEST_END = list(json_['epochs'].keys())[-1] # TODO
+
 
 main_df = pd.DataFrame.from_dict(json_['epochs'], orient='index')
 main_df.index = main_df.index.astype(int);
@@ -22,6 +29,9 @@ fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7) = plt.subplots(nrows=NUM_SUBPLOTS, ncol
 print('Total number of scanned TXs: {}'.format(main_df['num_txs_total'].sum()))
 
 ax1.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax1.axvspan(BULLSHARK_QUEST_2_START, BULLSHARK_QUEST_2_END, alpha=0.3, color='green', label='Bullshark Quest 2')
+ax1.axvspan(BULLSHARK_QUEST_3_START, BULLSHARK_QUEST_3_END, alpha=0.3, color='blue', label='Bullshark Quest 3')
+ax1.axvspan(WINTER_QUEST_START, WINTER_QUEST_END, alpha=0.3, color='cyan', label='Winter Quest')
 ax1.axhline(y=0, linestyle=':', linewidth=1, color='black')
 ax1.plot(main_df['num_txs_total'], linewidth=2, linestyle='--', marker='o', color='orange')
 ax1.set_title('Total number of TXs on the Sui mainnet per epoch')
@@ -30,6 +40,9 @@ ax1.minorticks_on()
 ax1.legend()
 
 ax2.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax2.axvspan(BULLSHARK_QUEST_2_START, BULLSHARK_QUEST_2_END, alpha=0.3, color='green', label='Bullshark Quest 2')
+ax2.axvspan(BULLSHARK_QUEST_3_START, BULLSHARK_QUEST_3_END, alpha=0.3, color='blue', label='Bullshark Quest 3')
+ax2.axvspan(WINTER_QUEST_START, WINTER_QUEST_END, alpha=0.3, color='cyan', label='Winter Quest')
 ax2.axhline(y=0, linestyle=':', linewidth=1, color='black')
 ax2.plot(main_df['num_txs_touching_shared_objs'], linewidth=2, linestyle='--', marker='o', color='magenta')
 ax2.set_title('Number of TXs touching shared objects on the Sui mainnet per epoch')
@@ -38,6 +51,9 @@ ax2.minorticks_on()
 ax2.legend()
 
 ax3.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax3.axvspan(BULLSHARK_QUEST_2_START, BULLSHARK_QUEST_2_END, alpha=0.3, color='green', label='Bullshark Quest 2')
+ax3.axvspan(BULLSHARK_QUEST_3_START, BULLSHARK_QUEST_3_END, alpha=0.3, color='blue', label='Bullshark Quest 3')
+ax3.axvspan(WINTER_QUEST_START, WINTER_QUEST_END, alpha=0.3, color='cyan', label='Winter Quest')
 ax3.axhline(y=0, linestyle=':', linewidth=1, color='black')
 ax3.plot(main_df['density'] * 100, linewidth=2, linestyle='--', marker='o', color='green')
 ax3.set_title('Percentage of TXs involving shared objects on the Sui mainnet per epoch')
@@ -46,6 +62,9 @@ ax3.minorticks_on()
 ax3.legend()
 
 ax4.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax4.axvspan(BULLSHARK_QUEST_2_START, BULLSHARK_QUEST_2_END, alpha=0.3, color='green', label='Bullshark Quest 2')
+ax4.axvspan(BULLSHARK_QUEST_3_START, BULLSHARK_QUEST_3_END, alpha=0.3, color='blue', label='Bullshark Quest 3')
+ax4.axvspan(WINTER_QUEST_START, WINTER_QUEST_END, alpha=0.3, color='cyan', label='Winter Quest')
 ax4.axhline(y=0, linestyle=':', linewidth=1, color='black')
 ax4.plot(main_df['num_shared_objects_per_epoch'], linewidth=2, linestyle='--', marker='o', color='olive')
 ax4.set_title('Number of (unique) shared objects on the Sui mainnet per epoch')
@@ -54,6 +73,9 @@ ax4.minorticks_on()
 ax4.legend()
 
 ax5.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax5.axvspan(BULLSHARK_QUEST_2_START, BULLSHARK_QUEST_2_END, alpha=0.3, color='green', label='Bullshark Quest 2')
+ax5.axvspan(BULLSHARK_QUEST_3_START, BULLSHARK_QUEST_3_END, alpha=0.3, color='blue', label='Bullshark Quest 3')
+ax5.axvspan(WINTER_QUEST_START, WINTER_QUEST_END, alpha=0.3, color='cyan', label='Winter Quest')
 ax5.axhline(y=0, linestyle=':', linewidth=1, color='black')
 ax5.plot(main_df['num_shared_objects_total'], linewidth=2, linestyle='--', marker='o', color='blue')
 ax5.set_title('Total number of (unique) shared objects on the Sui mainnet')
@@ -62,6 +84,9 @@ ax5.minorticks_on()
 ax5.legend()
 
 ax6.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax6.axvspan(BULLSHARK_QUEST_2_START, BULLSHARK_QUEST_2_END, alpha=0.3, color='green', label='Bullshark Quest 2')
+ax6.axvspan(BULLSHARK_QUEST_3_START, BULLSHARK_QUEST_3_END, alpha=0.3, color='blue', label='Bullshark Quest 3')
+ax6.axvspan(WINTER_QUEST_START, WINTER_QUEST_END, alpha=0.3, color='cyan', label='Winter Quest')
 ax6.axhline(y=0, linestyle=':', linewidth=1, color='black')
 ax6.axhline(y=1, linestyle='-.', linewidth=1, color='black')
 for col in interval_df:
@@ -73,6 +98,9 @@ ax6.minorticks_on()
 ax6.legend()
 
 ax7.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax7.axvspan(BULLSHARK_QUEST_2_START, BULLSHARK_QUEST_2_END, alpha=0.3, color='green', label='Bullshark Quest 2')
+ax7.axvspan(BULLSHARK_QUEST_3_START, BULLSHARK_QUEST_3_END, alpha=0.3, color='blue', label='Bullshark Quest 3')
+ax7.axvspan(WINTER_QUEST_START, WINTER_QUEST_END, alpha=0.3, color='cyan', label='Winter Quest')
 ax7.axhline(y=0, linestyle=':', linewidth=1, color='black')
 for col in interval_df:
     if not 'degree' in col:
