29f4d5857848827e08bd3fc8dc5149b4116618b0
Roman Overko <roman.overko@iota.org>
2024-02-23 15:20:06 +0100

    Update plot scripts and results until epoch 150

---
+++ results/until-epoch-150/plot-scripts/main.py
@@ -0,0 +1,87 @@
+import os
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+NUM_SUBPLOTS = 7
+FILE = os.path.join(os.pardir, 'epoch_density_data.json')
+with open(FILE, 'r') as f:
+    json_ = json.load(f);
+
+BULLSHARK_QUEST_1_START = 85
+BULLSHARK_QUEST_1_END = 106
+
+main_df = pd.DataFrame.from_dict(json_['epochs'], orient='index')
+main_df.index = main_df.index.astype(int);
+interval_df = pd.json_normalize(main_df['avg_interval_data'])
+
+plt.rcParams.update({'font.size': 14, 'font.family': 'sans-serif'})
+
+fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7) = plt.subplots(nrows=NUM_SUBPLOTS, ncols=1, figsize=(10, NUM_SUBPLOTS * 7))
+
+print('Total number of scanned TXs: {}'.format(main_df['num_txs_total'].sum()))
+
+ax1.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax1.axhline(y=0, linestyle=':', linewidth=1, color='black')
+ax1.plot(main_df['num_txs_total'], linewidth=2, linestyle='--', marker='o', color='orange')
+ax1.set_title('Total number of TXs on the Sui mainnet per epoch')
+ax1.set_ylabel('TX number')
+ax1.minorticks_on()
+ax1.legend()
+
+ax2.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax2.axhline(y=0, linestyle=':', linewidth=1, color='black')
+ax2.plot(main_df['num_txs_touching_shared_objs'], linewidth=2, linestyle='--', marker='o', color='magenta')
+ax2.set_title('Number of TXs touching shared objects on the Sui mainnet per epoch')
+ax2.set_ylabel('TX number')
+ax2.minorticks_on()
+ax2.legend()
+
+ax3.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax3.axhline(y=0, linestyle=':', linewidth=1, color='black')
+ax3.plot(main_df['density'] * 100, linewidth=2, linestyle='--', marker='o', color='green')
+ax3.set_title('Percentage of TXs involving shared objects on the Sui mainnet per epoch')
+ax3.set_ylabel('Density, %')
+ax3.minorticks_on()
+ax3.legend()
+
+ax4.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax4.axhline(y=0, linestyle=':', linewidth=1, color='black')
+ax4.plot(main_df['num_shared_objects_per_epoch'], linewidth=2, linestyle='--', marker='o', color='olive')
+ax4.set_title('Number of (unique) shared objects on the Sui mainnet per epoch')
+ax4.set_ylabel('Shared object number')
+ax4.minorticks_on()
+ax4.legend()
+
+ax5.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax5.axhline(y=0, linestyle=':', linewidth=1, color='black')
+ax5.plot(main_df['num_shared_objects_total'], linewidth=2, linestyle='--', marker='o', color='blue')
+ax5.set_title('Total number of (unique) shared objects on the Sui mainnet')
+ax5.set_ylabel('Shared object number')
+ax5.minorticks_on()
+ax5.legend()
+
+ax6.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax6.axhline(y=0, linestyle=':', linewidth=1, color='black')
+ax6.axhline(y=1, linestyle='-.', linewidth=1, color='black')
+for col in interval_df:
+    if 'degree' in col:
+        ax6.plot(interval_df[col], linewidth=2, label='Interval: {} checkpoints'.format(col.split('.')[0]))
+ax6.set_title('Average number of TXs touching the same shared object within an interval')
+ax6.set_ylabel('Avg contention degree')
+ax6.minorticks_on()
+ax6.legend()
+
+ax7.axvspan(BULLSHARK_QUEST_1_START, BULLSHARK_QUEST_1_END, alpha=0.3, color='red', label='Bullshark Quest 1')
+ax7.axhline(y=0, linestyle=':', linewidth=1, color='black')
+for col in interval_df:
+    if not 'degree' in col:
+        ax7.plot(interval_df[col], linewidth=2, label='Interval: {} checkpoints'.format(col.split('.')[0]))
+ax7.set_title('Average number of shared objects touched by more than one TX within an interval')
+ax7.set_xlabel('Epoch')
+ax7.set_ylabel('Avg object touchability')
+ax7.minorticks_on()
+ax7.legend()
+
+fig.tight_layout()
+plt.savefig(os.path.join(os.pardir, 'figure.png'), format='png')
---
+++ results/until-epoch-150/plot-scripts/obj_pie.py
@@ -0,0 +1,129 @@
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from pprint import pprint
+
+FILE = os.path.join(os.pardir, 'packages_data.json')
+with open(FILE, 'r') as f:
+    data = json.load(f);
+
+N_OBJS = 13
+OBJ_ID_NAME_MAP = {
+        '00b53b0f4174108627fbee72e2498b58d6a2714cded53fac537034c220d26302.price_info.PriceInfoObject': 'PriceInfoObject @Pyth Network',
+        '0000000000000000000000000000000000000000000000000000000000000002.clock.Clock': 'Clock* @Sui Framework',
+        '0000000000000000000000000000000000000000000000000000000000000002.kiosk.Kiosk': 'Kiosk @Sui Framework',
+        '5306f64e312b581766351c07af79c72fcb1cd25147157fdc2f8ad76de9a3fb6a.state.State': 'State* @Wormhole',
+        '00b53b0f4174108627fbee72e2498b58d6a2714cded53fac537034c220d26302.state.State': 'State* @Pyth Network',
+        'a0eba10b173538c8fecca1dff298e488402cc9ff374f8a12ca7758eebe830b66.spot_dex.Pool': 'Pool @Kriya DEX',
+        'd899cf7d2b5db716bd2cf55599fb0d5ee38a3061e7b6bb6eebf73fa5bc4c81ca.storage.Storage': 'Storage* @0xd...1ca',
+        'd899cf7d2b5db716bd2cf55599fb0d5ee38a3061e7b6bb6eebf73fa5bc4c81ca.pool.Pool': 'Pool @0xd...1ca',
+        '1eabed72c53feb3805120a081dc15963c204dc8d091542592abaf7a35689b2fb.pool.Pool': 'Pool @Cetus',
+        '1eabed72c53feb3805120a081dc15963c204dc8d091542592abaf7a35689b2fb.config.GlobalConfig': 'GlobalConfigig* @Cetus',
+        '745a16ea148a7b3d1f6e68d0f16237f954e99197cd0ffb96e70c994c946d60d1.coin_flip.Game': 'Game @DeSuiLabs Coin Flip',
+        '745a16ea148a7b3d1f6e68d0f16237f954e99197cd0ffb96e70c994c946d60d1.coin_flip.HouseData': 'HouseData* @DeSuiLabs Coin Flip',
+        'ceab84acf6bf70f503c3b0627acaff6b3f84cee0f2d7ed53d00fa6c2a168d14f': 'ABEx Core',
+        '91bfbc386a41afcfd9b2533058d7e915a1d3829089cc268ff4333d54d6339ca1': 'Turbos Finance',
+        'efe8b36d5b2e43728cc323298626b83177803521d195cfb11e15b910e892fddf': 'Scallop',
+        '000000000000000000000000000000000000000000000000000000000000dee9.clob_v2.Pool': 'Pool @DeepBook',
+        'Others': 'Others'
+}
+
+COLORS =  plt.cm.tab20((4./3*np.arange(20*3/4)).astype(int))
+
+total_num_txs = 0
+pkg_id_other_ids_map = {}
+scanned_pkgs = set()
+
+for pkg_id, pkg_data in data['packages'].items():
+    total_num_txs += pkg_data['total_num_txs']
+    if not pkg_id in scanned_pkgs:
+        scanned_pkgs.add(pkg_id)
+        pkg_id_other_ids_map[pkg_id] = []
+        for pkg_id2, pkg_data2 in data['packages'].items():
+            if not pkg_id == pkg_id2:
+                if pkg_data['types'].keys() == pkg_data2['types'].keys():
+                    scanned_pkgs.add(pkg_id2)
+                    pkg_id_other_ids_map[pkg_id].append(pkg_id2)
+
+obj_tx_num_map = {}
+for pkg_id, other_pkg_ids in pkg_id_other_ids_map.items():
+    for type_, type_data in data['packages'][pkg_id]['types'].items():
+        obj_tx_num_map['{}.{}'.format(pkg_id, type_)] = type_data['num_txs']
+
+    for id_ in other_pkg_ids:
+        for type_, type_data in data['packages'][id_]['types'].items():
+            obj_tx_num_map['{}.{}'.format(pkg_id, type_)] += type_data['num_txs']
+
+obj_tx_num_map = OrderedDict(sorted(obj_tx_num_map.items(), key=lambda x: x[1], reverse=True))
+
+i = 0
+obj_tx_num_map2 = OrderedDict()
+x = 0
+
+for id_, tx_num in obj_tx_num_map.items():
+    x += tx_num
+    if i < N_OBJS:
+        obj_tx_num_map2[id_] = tx_num
+        i += 1
+obj_tx_num_map2['Others'] = total_num_txs - sum(obj_tx_num_map2.values())
+# pprint(obj_tx_num_map2)
+
+plt.rcParams.update({
+    'font.size': 16,
+    'text.usetex': True,
+    'font.family': 'serif',
+    'font.serif': ['Times']
+})
+
+fig, (ax, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(10, 6), width_ratios=[3, 1], dpi=300)
+
+labels = []
+for k in obj_tx_num_map2.keys():
+    try:
+        labels.append(OBJ_ID_NAME_MAP[k])
+    except KeyError:
+        pass
+
+patches, texts, autotexts = ax.pie(obj_tx_num_map2.values(), labels=None, autopct='%1.1f\%%', startangle=40, pctdistance=0.8, colors=COLORS)
+
+bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+kw = dict(arrowprops=dict(arrowstyle="-"),
+          bbox=bbox_props, zorder=0, va="center")
+
+k = 0
+for i, patch in enumerate(patches):
+    if float(autotexts[i].get_text()[:-3]) < 5.0:
+        ang = (patch.theta2 - patch.theta1) / 2.0 + patch.theta1
+        x = np.cos(np.deg2rad(ang))
+        y = np.sin(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = f"angle,angleA=0,angleB={ang}"
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        if k == 0:
+            ax.annotate(autotexts[i].get_text(), xy=(x, y), xytext=(-0.9*np.sign(x) + 0.32*k, (1.05 + 0.17*k)*y),
+                    horizontalalignment=horizontalalignment, **kw)
+        elif k < 3:
+            ax.annotate(autotexts[i].get_text(), xy=(x, y), xytext=(-0.9*np.sign(x) + 0.32*k, (1.015 + 0.17*k)*y),
+                    horizontalalignment=horizontalalignment, **kw)
+        elif k < 4:
+            ax.annotate(autotexts[i].get_text(), xy=(x, y), xytext=(-0.345*np.sign(x) + 0.32*k, (1.00 + 0.15*k)*y),
+                    horizontalalignment=horizontalalignment, **kw)
+        elif k < 7:
+            ax.annotate(autotexts[i].get_text(), xy=(x, y), xytext=(0.2*np.sign(x) + 0.33*k, (1.62 - 0.01*k)*y),
+                    horizontalalignment=horizontalalignment, **kw)
+        else:
+            ax.annotate(autotexts[i].get_text(), xy=(x, y), xytext=(0.2*np.sign(x) + 0.101*k, (1.08 + 0.0*k)*y),
+                    horizontalalignment=horizontalalignment, **kw)
+        autotexts[i].set_visible(False)
+        k += 1
+
+ax2.set_frame_on(False)
+ax2.get_xaxis().set_visible(False)
+ax2.get_yaxis().set_visible(False)
+fig.legend(title='Shared object types:', labels=labels)
+
+fig.tight_layout()
+ax.set_position([-0.19, 0.04, 1.0, 1.0])
+plt.savefig(os.path.join(os.pardir, 'obj_pie.pdf'), format='pdf')
